e9b296d8be8b130656bf501eee00c3daf30b7115
parentroots <ebrahimnazmul20032@gmail.com>
2026-06-02 12:42:58 +0600

    cubit class practice

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:bloc_practice/features/user/presentation/screen/user_screen.dart';
+import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: UserScreen(),
+
+    );
+  }
+}
+
+
+
